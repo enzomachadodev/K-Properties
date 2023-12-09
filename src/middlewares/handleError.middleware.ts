f40712c7de4917d8d3cfc008a3ab7bcdbfea04d2
m4chado <enzo653fm@gmail.com>
2023-12-09 00:10:33 -0300

    refactor: rename handleError file to adhere to standards

--- src/middlewares/handleError.middleware.ts
+++ src/middlewares/handleError.middleware.ts
@@ -7,19 +7,16 @@ const handleError = async (
   error: Error,
   req: Request,
   res: Response,
-  next: NextFunction,
+  next?: NextFunction,
 ) => {
-  if (error instanceof AppError) {
+  if (error instanceof AppError)
     return res.status(error.statusCode).json({ message: error.message });
-  }
 
-  if (error instanceof ZodError) {
+  if (error instanceof ZodError)
     return res.status(400).json({ message: error.flatten().fieldErrors });
-  }
 
-  if (error instanceof JsonWebTokenError) {
+  if (error instanceof JsonWebTokenError)
     return res.status(401).json({ message: error.message });
-  }
 
   return res.status(500).json({
     message: "Internal server error!",
