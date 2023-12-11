60ae0cb53419bda88942fc943c18f9545dcbfe56
m4chado <enzo653fm@gmail.com>
2023-12-10 22:53:09 -0300

    feat: should not be able to create property with invalid categoryId

--- src/services/property.service.ts
+++ src/services/property.service.ts
@@ -1,7 +1,12 @@
+import { AppError } from "@/errors/AppError";
 import { IPropertyCreate } from "@/interfaces/properties.intefaces";
-import { propertyRepository } from "@/repositories";
+import { categoryRepository, propertyRepository } from "@/repositories";
 
 const create = async (payload: IPropertyCreate) => {
+  const category = await categoryRepository.findOneBy({
+    id: payload.categoryId,
+  });
+  if (!category) throw new AppError("Category not found", 404);
   let newProperty = propertyRepository.create(payload);
   newProperty = await propertyRepository.save(newProperty);
   const property = await propertyRepository.findOne({
