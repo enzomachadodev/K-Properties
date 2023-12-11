f2b7fdede52da16f2722567fd9485f7079dd1e98
m4chado <enzo653fm@gmail.com>
2023-12-10 23:14:43 -0300

    feat: should be able to create a schedule

--- src/controllers/schedule.controller.ts
+++ src/controllers/schedule.controller.ts
@@ -1,8 +1,8 @@
 import { scheduleService } from "@/services";
 import { Request, Response } from "express";
 const create = async (req: Request, res: Response) => {
-  const newSchedule = await scheduleService.create(req.body);
-  return res.status(200).json(newSchedule);
+  await scheduleService.create(req.body);
+  return res.status(201).json({ message: "successfully scheduled time" });
 };
 
 const readByPropertyId = async (req: Request, res: Response) => {
